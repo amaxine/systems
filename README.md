c69cc1a26fb7fd905c85d846f585e89ba2877628
Maxine E. Aubrey <maxeaubrey@gmail.com>
2020-06-03 13:58:11 +0200

    omnia: initial configuration

--- README.md
+++ README.md
@@ -7,3 +7,15 @@ This repository stores the nix configurations for various machines.
 | name | description |
 | ---- | ----------- |
 | chirm | Dell XPS 13 7390 |
+| omnia | Intel Core i7-8559U |
+
+## Handy reminders
+
+ZFS
+
+```
+zpool create -O mountpoint=none -O atime=off -O compression=lz4 -O xattr=sa -O acltype=posixacl rpool /dev/disk/by-id/...-part1
+zfs create -o mountpoint=legacy rpool/root
+zfs create -o mountpoint=legacy rpool/home
+zfs create -o sync=disabled -o setuid=off -o devices=off -o mountpoint=legacy rpool/tmp
+```
---
+++ hosts/omnia/configuration.nix
@@ -0,0 +1,41 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+      ../../modules/base
+    ];
+
+  networking.hostName = "omnia";
+  networking.hostId = "080f5139";
+
+  boot.loader = {
+    systemd-boot = {
+      configurationLimit = 10;
+      enable = true;
+      editor = false;
+    };
+    timeout = 3;
+    efi.canTouchEfiVariables = true;
+  };
+
+  networking.interfaces.eno1.useDHCP = true;
+  networking.interfaces.wlp0s20f3.useDHCP = true;
+
+  time.timeZone = "UTC";
+
+  services.openssh.enable = true;
+  networking.firewall.allowedTCPPorts = [ 22 ];
+
+  users.users.maxine = {
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKDmex7rvB7BFd9OxQHfgqKogiN69kHvixCzWWEGh5oY maxine@chirm"
+    ];
+  };
+
+  security.sudo.wheelNeedsPassword = false;
+
+  system.stateVersion = "20.09";
+}
+
