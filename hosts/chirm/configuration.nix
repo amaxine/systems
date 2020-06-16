2bc8c970485cf12a206fef1be7f5314310680d2b
Maxine E. Aubrey <maxeaubrey@gmail.com>
2020-06-16 16:50:26 +0200

    chirm: home NAS mount

--- hosts/chirm/configuration.nix
+++ hosts/chirm/configuration.nix
@@ -11,6 +11,12 @@
   networking.hostName = "chirm";
   networking.hostId = "c53a6417";
 
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [ 9309 ];
+    allowedUDPPorts = [ 9309 ];
+  };
+
   boot.loader = {
     systemd-boot = {
       configurationLimit = 10;
@@ -28,6 +34,12 @@
     swap = {
       device = "/dev/nvme0n1p2";
     };
+  };
+
+  fileSystems."/home/maxine/NAS" = {
+    device = "192.168.178.105:spool/home/maxine";
+    fsType = "nfs";
+    options = ["x-systemd.automount" "noauto"];
   };
 
   boot.kernelParams = [ "mem_sleep_default=deep" ];
