1c91713234e2e0206745570f941cce37f9fa888e
Maxine Aubrey <maxeaubrey@gmail.com>
2021-05-08 13:12:10 +0200

    m/remote-builds/user: more builders

--- modules/remote-builds/user/default.nix
+++ modules/remote-builds/user/default.nix
@@ -7,8 +7,22 @@
         hostName = "ssh://remote-build-user@omnia.fritz.box";
         system = "x86_64-linux";
         maxJobs = 8;
+        speedFactor = 2;
         supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
-        mandatoryFeatures = [ ];
+      }
+      {
+        hostName = "ssh://remote-build-user@sintra.fritz.box";
+        system = "x86_64-linux";
+        maxJobs = 24;
+        speedFactor = 5;
+        supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" ];
+      }
+      {
+        hostName = "ssh://remote-build-user@berlin.infra.hormonal.party";
+        system = "x86_64-linux";
+        maxJobs = 12;
+        speedFactor = 2;
+        supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" ];
       }
     ];
 
