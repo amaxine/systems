77cfbe68574010f4efb790e01c46d81677c39004
Maxine Aubrey <maxeaubrey@gmail.com>
2021-07-27 17:10:16 +0200

    m/base: use systemd-resolved

--- modules/base/default.nix
+++ modules/base/default.nix
@@ -14,6 +14,7 @@
   networking.useDHCP = false;
   networking.networkmanager.enable = true;
   networking.networkmanager.wifi.backend = "iwd";
+  services.resolved.enable = true;
 
   nixpkgs.config.allowUnfree = true;
 
