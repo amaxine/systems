7121e1957c278070a7f6821ef01afa48de41204c
Maxine Aubrey <maxeaubrey@gmail.com>
2022-06-18 15:20:33 +0200

    m/graphical: switch to pipewire

--- modules/graphical/default.nix
+++ modules/graphical/default.nix
@@ -3,8 +3,15 @@
 {
   boot.plymouth.enable = true;
 
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
+  sound.enable = false;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
 
   hardware.keyboard.zsa.enable = true;
 
