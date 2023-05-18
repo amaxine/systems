c5cdb9945ee983425a62797fd1a40ce53769977b
Maxine Aubrey <maxeaubrey@gmail.com>
2023-05-19 01:53:39 +0200

    m/remote-builds/user: retire berlin

--- modules/remote-builds/user/default.nix
+++ modules/remote-builds/user/default.nix
@@ -3,13 +3,6 @@
 {
   nix = {
     buildMachines = [
-      {
-        hostName = "remote-build-user@berlin.infra.hormonal.party";
-        system = "x86_64-linux";
-        maxJobs = 12;
-        speedFactor = 2;
-        supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" ];
-      }
     ];
 
     distributedBuilds = true;
