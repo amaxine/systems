0ec4fdcf16424fcea18fdaadd1575bcbd29a0832
Maxine Aubrey <maxeaubrey@gmail.com>
2023-05-19 01:53:54 +0200

    m/remote-builds/user: add tsort for arm64

--- modules/remote-builds/user/default.nix
+++ modules/remote-builds/user/default.nix
@@ -3,6 +3,13 @@
 {
   nix = {
     buildMachines = [
+      {
+        hostName = "remote-build-user@tsort.infra.hormonal.party";
+        system = "aarch64-linux";
+        maxJobs = 4;
+        speedFactor = 1;
+        supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+      }
     ];
 
     distributedBuilds = true;
